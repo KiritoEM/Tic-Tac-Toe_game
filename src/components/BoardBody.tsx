2ee39cd845bccca10d69775837ad658852eaa79a
kiritoEM <johankirito64@gmail.com>
2023-12-29 20:34:23 +0100

    add current Player turn

--- src/components/BoardBody.tsx
+++ src/components/BoardBody.tsx
@@ -9,13 +9,24 @@ const BoardBody = (): JSX.Element => {
     [null, null, null],
     [null, null, null],
   ];
+
   const [board, setBoard] = useState<Player[][]>(defaultBoard);
+  const [currentPlayer, setCurrentPlayer] = useState<number>(1);
+
+  const getCurrentPlayer = (): number => {
+    currentPlayer===1 ? setCurrentPlayer(2) : setCurrentPlayer(1);
+    return currentPlayer;
+  };
   return (
     <section id="board__body">
       {board.map((row, rowIndex) => (
         <div key={rowIndex} className={`section-${rowIndex + 1}`}>
           {row.map((value, colIndex) => (
-            <BoardSquare key={colIndex} />
+            <BoardSquare
+              key={colIndex}
+              value={value}
+              getPlayer={getCurrentPlayer}
+            />
           ))}
         </div>
       ))}
--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -1,7 +1,18 @@
-const BoardSquare = () : JSX.Element => {
-    return (
-            <div className="board-square"></div>
-    );
+interface Iaction {
+  getPlayer: () => number;
+  value: number | null;
+}
+
+const BoardSquare = ({ value, getPlayer }: Iaction): JSX.Element => {
+  return (
+    <div
+      className="board-square"
+      onClick={() => {
+        console.log("value: ", value);
+        console.log("current-player : ", getPlayer());
+      }}
+    ></div>
+  );
 };
 
 export default BoardSquare;
